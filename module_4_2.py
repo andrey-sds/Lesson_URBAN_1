fb9e34b1412fc2133f38e89ea15fea5d8b64df77
andrey-sds <dz_andrey@mail.ru>
2024-08-08 14:15:15 +0300

    Add files via upload

--- module_4_2.py
+++ module_4_2.py
@@ -1,9 +1,3 @@
-# Создайте новую функцию def test_function
-# Создайте другую функцию внутри функции inner_function,
-# функция должна печатать значение "Я в области видимости функции test_function"
-# Вызовите функцию inner_function внутри функции test_function
-# Попробуйте вызывать inner_function вне функции test_function и посмотрите на результат выполнения программы
-
 def test_function(val):
     # global pr_  # изменение значения глобальной переменной pr_
     pr_ = "Я в функции test_function"
