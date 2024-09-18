27050ab1539db5c9ce10f2414dfcaf1cb27f46ef
andrey-sds <dz_andrey@mail.ru>
2024-09-18 16:00:09 +0300

    Update module_8_1.py

--- module_8/module_8_1.py
+++ module_8/module_8_1.py
@@ -4,9 +4,9 @@ def add_everything_up(a, b):
     except TypeError as exc:
         print(f'Одно из значений не является числом {exc}: a - {type(a)}; b - {type(b)} ')
         return f'Склеиваем a и b как строки: {str(a) + str(b)}'
-    else:
-        print(f'Обе переменные - числа a - {type(a)}; b - {type(b)}')
-        return f'Сумма чисел: {round(a + b,3)}'
+   
+    print(f'Обе переменные - числа a - {type(a)}; b - {type(b)}')
+    return f'Сумма чисел: {round(a + b,3)}'
 
 
 print(add_everything_up(123.456, 'строка'))
