496a8ecb0f6258a4ee102a49d7299f8009b260d1
andrey-sds <dz_andrey@mail.ru>
2024-09-26 19:40:18 +0300

    Add files via upload

--- module_9/module_9_3.py
+++ module_9/module_9_3.py
@@ -1,33 +1,8 @@
-from random import choice
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
 
+first_result = (len(x[0]) - len(x[1]) for x in zip(first, second) if len(x[0]) != len(x[1]))
+second_result = (True if len(first[x]) == len(second[x]) else False for x in range(len(first)))
 
-class MysticBall:
-
-    def __init__(self, *words):
-        self.words = words
-
-    def __call__(self, *words):
-        return choice(self.words)
-
-
-def get_advanced_writer(file_name):
-    def write_everything(*data_set):
-        with open(file_name, 'w', encoding='utf-8') as file:
-            for i in data_set:
-                file.write(str(i)+'\n')
-    return write_everything
-
-
-first = 'Мама мыла раму'
-second = 'Рамена мало было'
-
-print(list(map(lambda x, y: y in x, first, second)))
-
-write = get_advanced_writer('example.txt')
-write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
-
-
-first_ball = MysticBall('Да', 'Нет', 'Наверное', 'Звезды говорят - Да')
-print(first_ball())
-print(first_ball())
-print(first_ball())
+print(list(first_result))
+print(list(second_result))
