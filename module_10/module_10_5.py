ee8c52ee30e7083cb763e55eb0e67c8a63396d88
andrey-sds <dz_andrey@mail.ru>
2024-10-08 14:14:38 +0300

    Add files via upload

---
+++ module_10/module_10_5.py
@@ -0,0 +1,32 @@
+import multiprocessing
+import datetime
+
+
+def read_info(name):
+    all_data = []
+    with open(name, 'r', encoding='utf-8') as file:
+        while True:
+            content = file.readline()
+            if not content:
+                break
+            else:
+                all_data.append(content)
+                # print(all_data)
+
+
+filenames = [f'Files/file {number}.txt' for number in range(1, 5)]
+# start_time = datetime.datetime.now()
+# for i in filenames:
+#     read_info(i)
+# end_time = datetime.datetime.now()
+# print(end_time - start_time)
+
+# многопроцессность
+if __name__ == '__main__':
+    with multiprocessing.Pool(processes=4) as pool:
+        start_time = datetime.datetime.now()
+        pool.map(read_info, filenames)
+    end_time = datetime.datetime.now()
+    print(end_time - start_time)
+
+
