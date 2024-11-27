c2c53775d1a06928dde3fb95953f51744310d6ff
andrey-sds <dz_andrey@mail.ru>
2024-11-27 22:04:39 +0300

    Add files via upload

--- module_14/crud_functions.py
+++ module_14/crud_functions.py
@@ -1,6 +1,10 @@
 import sqlite3
 
 
+conn = sqlite3.connect("shop.db")
+cursor = conn.cursor()
+
+
 def initiate_db():
     connection = sqlite3.connect("shop.db")
     cursor = connection.cursor()
@@ -13,16 +17,45 @@ def initiate_db():
     );
     ''')
     connection.commit()
+    cursor.execute(''' 
+        CREATE TABLE IF NOT EXISTS Users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT NOT NULL,
+        email TEXT NOT NULL,
+        age INTEGER NOT NULL,
+        balance INTEGER
+        );
+        ''')
+    connection.commit()
     connection.close()
 
 
 def get_all_products(product_id):
-    with sqlite3.connect('shop.db') as connection:
-        cursor = connection.cursor()
-        products = cursor.execute("SELECT  title, description, price FROM Products WHERE id = ?", (product_id,))
-        product = cursor.fetchone()
+    # with sqlite3.connect('shop.db') as connection:
+    # cursor = conn.cursor()
+    products = cursor.execute("SELECT  title, description, price FROM Products WHERE id = ?", (product_id,))
+    product = cursor.fetchone()
     return product
 
+
+def add_user(username, email, age):
+    check_in_db = is_included(username)
+    if check_in_db is True:
+        cursor.execute(f'''
+                INSERT INTO Users (username, email, age, balance) VALUES ('{username}', '{email}', '{age}', 1000)
+                ''')
+        conn.commit()
+    else:
+        return f'Пользователь с таким именем уже существует!'
+
+
+def is_included(username):
+    cursor.execute(f"SELECT id FROM Users WHERE username = ?", (username,))
+    check_username = cursor.fetchone()
+    if check_username is None:
+        return True
+    else:
+        return False
 # cursor.execute("INSERT INTO Products(title, description, price) VALUES ('Витамин Д3+К2', 'Витамины в таблетках', 100);")
 # connection.commit()
 # cursor.execute("INSERT INTO Products(title, description, price) VALUES ('Омега-3', 'Капсулы', 200);")
@@ -32,6 +65,8 @@ def get_all_products(product_id):
 # cursor.execute("INSERT INTO Products(title, description, price) VALUES ('Комплекс витаминов', 'Набор витаминов для здоровья', 400);")
 
 
+if __name__ == '__main__':
+    initiate_db()
 
 
 
